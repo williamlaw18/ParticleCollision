9d4fe0459526650822335574b903bb61caf3af3d
Will Law <41906067+williamlaw18@users.noreply.github.com>
2022-05-30 15:51:28 +0100

    added object collision detection

--- src/objects.jsx
+++ src/objects.jsx
@@ -13,7 +13,7 @@ const Objects = (
 
     const initCanvas = () => {
         setCanvasArea();
-        createObject(20, 20, 150, 150, [0.3, 0.5])
+        createCircle(100, 100, 50, [0.3, 0.5])
         window.addEventListener('resize', setCanvasArea);
         animateRef.current = requestAnimationFrame(animate);
     }
@@ -27,15 +27,44 @@ const Objects = (
     }
 
     //adds object to list
-    const createObject = (x, y, width, height, vector) => {
+    const createCircle = (x, y, radius, vector) => {
         const object = {
+            type: 'circle',
             x: x,
             y: y,
-            width: width,
-            height: height,
-            vector: vector
+            radius: radius,
+            vector: vector,
+            isColliding: false
         };
         objects.current = [...objects.current, object]
+    }
+
+    const circIntersect = (x1, y1, r1, x2, y2, r2) => {
+        // Calculate the distance between the two circles
+        let squareDistance = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
+        // if distance is smaller or equal to the sum two radius
+        return squareDistance <= ((r1 + r2) * (r1 + r2))
+    }
+
+    const detectCollisions = () => {
+        let obj2;
+        // Reset collision state of all objects
+        objects.current.forEach((object) => {
+            object.isColliding = false;
+        });
+    
+        // Start checking for collisions
+        objects.current.forEach((obj1, index) => {
+            for (let j = index + 1; j < objects.current.length; j++){
+                obj2 = objects.current[j];
+    
+                // Compare object1 with object2
+                if (circIntersect(obj1.x, obj1.y, obj1.radius, obj2.x, obj2.y, obj2.radius)) {
+                    obj1.isColliding = true;
+                    obj2.isColliding = true;
+                }
+            }
+        });
     }
 
     //draws object on canvas
@@ -48,16 +77,27 @@ const Objects = (
             object.x = object.x + object.vector[0]
             object.y = object.y + object.vector[1]
 
+            console.log(object.isColliding)
+
             ctx.beginPath();
-            ctx.rect(object.x, object.y, object.width, object.height);
+            ctx.arc(object.x, object.y, object.radius, 0, 2 * Math.PI, false)
             ctx.stroke();
         })
+        
     };
 
     //Draw animation frames
     const animate = () => {
+        detectCollisions();
         drawObjects();
-        animateRef.current = requestAnimationFrame(animate);
+        // animateRef.current = requestAnimationFrame(animate);
+    }
+    setInterval(() => {
+        animate()
+    }, 100);
+
+    const handleCanvasClick = (e) => {
+        createCircle(e.nativeEvent.offsetX, e.nativeEvent.offsetY, 50, [-0.3, 0.5])
     }
 
     //Component update
@@ -72,6 +112,7 @@ const Objects = (
     return (
         <React.Fragment>
             <canvas
+                onMouseDown={handleCanvasClick}
                 ref={canvasRef}
                 width={`${canvasWidth}px`}
                 height={`${canvasHeight}px`}
